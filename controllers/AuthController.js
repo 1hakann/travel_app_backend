359505b3159c9dd9299086ac6caf3820bb5a39e9
Hakan Çakmak <hakan.cakmak@live.com>
2024-02-01 21:55:00 +0300

    created user function.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,7 +9,15 @@ module.exports = {
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: CryptoJS.AES.encrypt(req.body.password, )
-        })
+            password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET).toString(),
+        });
+
+        try {
+            await newUser.save();
+
+            res.status(201).json({status: true, message: "User successfully created"})
+        } catch(error) {
+
+        }
     }
 }
