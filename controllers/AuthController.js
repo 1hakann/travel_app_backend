750f8f0b5d9afc3f17aefec2c7406e53ae8090b0
Hakan Çakmak <hakan.cakmak@live.com>
2024-02-01 22:18:01 +0300

    login function have been written.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -19,5 +19,37 @@ module.exports = {
         } catch(err) {
             return next(err)
         }
+    },
+
+    loginUser: async (res, req, next) => {
+        try {
+            const user = await User.findOne({email: req.body.email})
+
+            if(!user) {
+                return res.status(401).json({status: false, message: "User not found"});
+            }
+
+            const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET);
+            const decryptString = decryptedPassword.toString(CryptoJS.enc.Utf8);
+
+            if(decryptString !== req.body.password) {
+                return res.status(401).json({status: false, message: "Wrong password"});
+            }
+
+            const userToken = jwt.sign(
+                {
+                    id: user.id,
+                },
+                process.env.JWT_SECRET, {expiresIn: "21d"}
+            );
+
+            const user_id = user.id;
+
+            res.status(200).json({status: true, id: user_id, token: userToken})
+
+        } catch(err) {
+            return next(err)
+        }
     }
+
 }
