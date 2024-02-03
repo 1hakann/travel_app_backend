dffbf72b593a3e1b84f5700bd7d8b70b87c73865
Hakan Çakmak <hakan.cakmak@live.com>
2024-02-03 13:51:26 +0300

    007 - created auth router.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const errorHandler = require('./middleware/ErrorHandling')
+authRouter = require("./routes/auth");
 const port = 3000
 
 dotenv.config()
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json({limit: "10mb"}))
 app.use(express.urlencoded({limit: "10mb", extended: true}))
 app.use(errorHandler)
+
+// localhost:5003/api/register
+app.use('/api', authRouter)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
